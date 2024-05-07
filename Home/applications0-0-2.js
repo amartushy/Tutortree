2df73b2cf913e5c6b31ac32329a1ca391b13599c
Adrian Martushev <55608214+amartushy@users.noreply.github.com>
2024-05-06 20:22:06 -0700

    Update and rename applications0-0-1.js to applications0-0-2.js

--- Home/applications0-0-2.js
+++ Home/applications0-0-2.js
@@ -67,7 +67,7 @@ function renderApplications(applications) {
         if (app.tutorApplicationStatus == "pending") {
             let approveButton = createDOMElement('div', 'approve-button', 'Approve', actionsContainer);
             approveButton.addEventListener('click', () => {
-                sendTutorApprovalData(app.email, app.name);
+                approveTutor(app.id, app)
                 console.log(app.email, app.name)
             });
             let declineButton = createDOMElement('div', 'decline-button', 'Decline', actionsContainer);
@@ -78,74 +78,5 @@ function renderApplications(applications) {
         }
         
     });
-}
-
-function sendTutorApprovalData(userEmail, userName) {
-    fetch('https://tutortree-1f6f9e7e11c7.herokuapp.com/sendTutorAcceptanceEmail', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            user_email: userEmail,
-            user_name: userName
-        })
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok: ' + response.statusText);
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
-}
-
-
-
-//Helper functions
-function formatDate(timestamp) {
-    const options = { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' };
-    return parseDate(timestamp).toLocaleDateString('en-US', options);
-}
-
-function getInitials(fullName) {
-    // Check if fullName is undefined or empty
-    if (!fullName) {
-        return ""; // Return an empty string or any default initials you prefer
-    }
-    
-    const names = fullName.split(' ');
-    let initials = names.map(name => {
-        // Check if name is not empty to avoid errors
-        if (name) {
-            return name[0].toUpperCase();
-        }
-        return ''; // Return empty string if name part is undefined or empty
-    }).join('');
-    
-    return initials.slice(0, 2);
-}
-
-
-// Improved parseDate function to safely handle undefined values
-function parseDate(date) {
-    if (!date) {
-        // Handle undefined or null dates by returning a default old date
-        return new Date(0);
-    } else if (typeof date === 'number') {
-        // Assuming the number is milliseconds since the epoch
-        return new Date(date);
-    } else if (date.seconds) {
-        // Handle Firestore timestamp
-        return new Date(date.seconds * 1000);
-    } else {
-        // Return a default old date if the format is unknown
-        return new Date(0);
-    }
 }
 
